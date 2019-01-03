17287eba5a48f6d9f31981234ac30702450b34df
Takafumi Arakaki <aka.tkf@gmail.com>
2019-01-02 16:47:18 -0800

    Test basic interface for SparseXXVector

--- test/test_vector.jl
+++ test/test_vector.jl
@@ -1,6 +1,16 @@
 module TestVector
 include("preamble.jl")
 
+@testset "basic interface" begin
+    xs0 = sprandn(100, 0.3)
+    xs = SparseXXVector(xs0)
+
+    @test xs[1] isa Float64
+    @test xs == xs0
+    @test sprint(show, xs) isa String
+    @test sprint(show, "text/plain", xs) isa String
+end
+
 @testset begin
     xs0 = sprandn(100, 0.3)
     xs = SparseXXVector(xs0)
